8f0c17b41dbccfe8a20ed298c62121eba982e973
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-02-07 11:20:14 +0000

    Activity 5 complete

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import CharacterContainer from './components/character_container';
 import Navigation from './components/navigation';
 import { DisneyCharacter } from './disney_character';
 
+export const FavouritesContext = React.createContext<number[]>([]);
+
 const App : React.FC = () => {
 
 	const [currentPage, setCurrentPage] = useState<number>(1);
-
   const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
-
   const [characterFavourites, setCharacterFavourites] = useState<Array<number>>([]);
   
   const getCharacters = async (pageNumber : number) => {
@@ -25,15 +25,16 @@ const App : React.FC = () => {
   }, [currentPage]);
 
   return (
-    <div className="page">
-      <Header currentPage={currentPage} />
-      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <CharacterContainer 
-        characters={characters}
-        characterFavourites={characterFavourites}
-        updateFavourites={setCharacterFavourites}
-      />
-    </div>
+    <FavouritesContext.Provider value={characterFavourites}>
+      <div className="page">
+        <Header currentPage={currentPage} />
+        <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        <CharacterContainer 
+          characters={characters}          
+          updateFavourites={setCharacterFavourites}
+        />
+      </div>
+    </FavouritesContext.Provider>
   );
 }
 
